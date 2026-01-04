1125fef957b54da3d727376a09bae80e43d6b6bc
Aman Bansal <bansalaman2419@gmail.com>
2026-01-04 23:16:38 +0530

    Add files via upload

---
+++ Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    if(a%2==0){
+        cout<<"Mahmoud"<<endl;
+    }
+    else if(a%2!=0){
+        cout<<"Ehab"<<endl;
+    }
+    return 0;
+}
